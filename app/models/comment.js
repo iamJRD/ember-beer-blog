5d3146dffd39e7802c0f49acdef32c7194e4cdcb
jared beckler and yvonna contreras <yvonnap.contreras@gmail.com>
2016-03-31 11:31:54 -0700

    add minor styling to admin page

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   body: DS.attr(),
-  posts: DS.belongsTo('post', {async: true});
+  posts: DS.belongsTo('post', {async: true})
 });
