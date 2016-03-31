a24fab73680e6978eca7db790b3c94b655e9f272
jared beckler and yvonna contreras <yvonnap.contreras@gmail.com>
2016-03-31 11:03:25 -0700

    Create comments model

--- app/models/comments.js
+++ app/models/comments.js
@@ -2,8 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
-  author: DS.attr(),
   body: DS.attr(),
-  image: DS.attr(),
-  comments: DS.hasMany('comment', {async: true})
+  posts: DS.belongsTo('post', {async: true});
 });
